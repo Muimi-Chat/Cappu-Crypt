23326bee8526e4a8937e16862e1f5672cf6c57b1
WQYeo <yeo_dev@protonmail.com>
2024-06-26 22:05:14 +0800

    Init Project

---
+++ src/main/java/wqyeo/cappucrypt/CappuCryptApplication.java
@@ -0,0 +1,13 @@
+package wqyeo.cappucrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CappuCryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CappuCryptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wqyeo/cappucrypt/CappuCryptApplicationTests.java
@@ -0,0 +1,13 @@
+package wqyeo.cappucrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CappuCryptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
